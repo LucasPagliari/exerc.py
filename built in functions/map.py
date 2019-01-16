f2dc7b4697c3384d675bf7c034ef9f7752056be6
Lucas Pagliari <lucas.alex.pagliari@gmail.com>
2019-01-16 12:43:43 -0200

    .

---
+++ built in functions/map.py
@@ -0,0 +1,25 @@
+lista = []
+
+for i in range(0,20):
+    lista.append(i)
+
+print(lista)
+print('\n')
+
+def square(x):
+    return (x**2)
+x = list(map(square,lista))
+print(x)
+print('\n')
+
+x = list(map((lambda x: x**3), lista))
+
+print(x)
+print('\n')
+
+a = [1, 2, 3] 
+b = [4, 5, 6]
+c = [7, 8, 9]
+
+x = list(map(lambda x,y,z: x+y+z, a,b,c))
+print(x)
---
+++ modulos e pacotes.py
@@ -0,0 +1,13 @@
+import urllib.request
+
+resposta = urllib.request.urlopen('http://www.fatecjd.edu.br/portal/')
+
+print (resposta)
+
+html = resposta.read()
+i = 0
+for x in html:
+    if i < 100:
+        print(x)
+        
+print(html)
